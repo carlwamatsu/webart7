253b5cf9940c7377ce377a2bb635aaa01508e362
carlwamatsu <carlosmagno201@gmail.com>
2024-08-19 17:09:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,6 +39,48 @@ document.addEventListener('DOMContentLoaded', function() {
     // Seleciona aleatoriamente 3 imagens no carregamento da página
     const selectedGifs = getRandomImages(gifs, 3);
 
+    let isTouchActive = false; // Variável para verificar o estado do toque
+
+    // Evento que é chamado quando o toque é iniciado
+    document.addEventListener('touchstart', function(event) {
+        isTouchActive = true;
+    });
+
+    // Evento que é chamado quando o toque é terminado
+    document.addEventListener('touchend', function(event) {
+        isTouchActive = false;
+    });
+
+    // Evento que é chamado quando o toque é movido
+    document.addEventListener('touchmove', function(event) {
+        if (isTouchActive) { // Verifica se o toque está ativo
+            // Previne o comportamento padrão do navegador, como o scrolling
+            event.preventDefault();
+
+            // Obtém as coordenadas do toque
+            const touch = event.touches[0];
+            const touchX = touch.pageX;
+            const touchY = touch.pageY;
+
+            // Seleciona um GIF aleatório das 3 imagens selecionadas
+            let gifIndex = Math.floor(Math.random() * selectedGifs.length);
+
+            // Cria um novo elemento de imagem
+            let img = document.createElement('img');
+            img.src = selectedGifs[gifIndex]; // Atribui o GIF selecionado
+            img.classList.add('plant');
+
+            // Define a posição da imagem baseada na posição do toque
+            img.style.position = 'absolute'; // Garantir que a imagem seja posicionada corretamente
+            img.style.left = touchX + 'px';
+            img.style.top = touchY + 'px';
+
+            // Adiciona a imagem ao container
+            container.appendChild(img);
+        }
+    });
+
+    // Eventos de mouse para suportar desktops
     let isMouseDown = false; // Variável para verificar o estado do botão do mouse
 
     // Evento que é chamado quando o botão do mouse é pressionado
@@ -51,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function() {
         isMouseDown = false;
     });
 
-    // Adiciona o evento de movimento do mouse
+    // Evento que é chamado quando o mouse é movido
     document.addEventListener('mousemove', function(event) {
         if (isMouseDown) { // Verifica se o botão do mouse está pressionado
             // Seleciona um GIF aleatório das 3 imagens selecionadas
